ffeb0702af2e7466967d9eb760297b589598f1ca
vidya91482 <vidyamagi03215@gmail.com>
2024-04-19 18:04:40 +0530

    comitted 1st time

---
+++ CRM/src/test/java/test/DemoJenkins.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkins {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	@Test
+  public void test1(){
+		System.out.println("i have created new project");
+		
+
+	}
+
+}
